b47e3a2523ac113d10b55bb20779261a9388cc74
alysonabril <alysonabril@pursuit.org>
2019-08-07 14:13:03 -0400

    first commit, problem 1 completed

---
+++ autoLayout/Control/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Alyson Abril on 8/7/19.
+//  Copyright © 2019 Alyson Abril. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
